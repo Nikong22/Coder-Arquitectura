de73cb9d8aa951edc48eea15aaf03e7d7fad0735
Nikong <nico.alejandro20@gmail.com>
2022-09-07 04:46:58 -0300

    first commit

---
+++ options/mongoDB.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const logger = require('../');
+
+const URI = 'mongodb://localhost:27017/coder';
+
+const optionsDB = mongoose.connect(URI,
+  {
+    serverSelectionTimeoutMS: 5000
+  },
+  (error) => {
+    if (error) {
+      throw 'Error al conectarse a la base de datos';
+    }
+  });
+
+module.exports = {
+  optionsDB
+}
